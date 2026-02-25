8912712c5e7e459f551fb60b5f405a8b6729022e
Antoine <antoine.bugeia@gouv.nc>
2026-02-25 14:08:36 +1100

    feat: add compose qualif & API_URL env var

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
-let API_URL = "https://localisation.gouv.nc/api/"
+let API_URL = import.meta.env.API_URL || "https://localisation.gouv.nc/api/"
 /** 
 if(window.location.origin.indexOf("carto.valid-province-nord.nc") != -1){
     API_URL = "";
@@ -18,39 +18,39 @@ const _base_options = {
     indexes: ["address", "poi", "cadastre"],
     communes: null,
     limit: 10
-} 
+}
 
 export const getAutocompleteTypes = (indexes) => {
     let types = [];
-    for(let t in AUTOCOMPLETE_INDEXES){
+    for (let t in AUTOCOMPLETE_INDEXES) {
         const idx = AUTOCOMPLETE_INDEXES[t];
-        if(indexes.indexOf(idx) != -1){
+        if (indexes.indexOf(idx) != -1) {
             types.push(t);
         }
     }
     return types;
 }
 
-export class ApiNcLocalisation{
-    indexes = _base_options.indexes.map(idx=>idx);
+export class ApiNcLocalisation {
+    indexes = _base_options.indexes.map(idx => idx);
     options = {};
     url = _base_options.url;
     capabilities = null;
 
     constructor(options) {
-        if(!options){
+        if (!options) {
             options = {}
         }
-        this.options = {..._base_options, ...options};
+        this.options = { ..._base_options, ...options };
         this.url = this.options.url;
-        if(!this.url.endsWith("/")){
+        if (!this.url.endsWith("/")) {
             this.url += "/";
         }
 
-        if(this.options.indexes){
+        if (this.options.indexes) {
             this.indexes = this.options.indexes;
         }
-        
+
 
         this.load();
     }
@@ -59,88 +59,88 @@ export class ApiNcLocalisation{
         this.options.limit = limit;
     }
 
-    setUrl = async(url) => {
+    setUrl = async (url) => {
         const previous = this.url;
 
-        try{
+        try {
             this.url = url;
             await this.load(true);
         }
-        catch(error){
+        catch (error) {
             console.warn("Failed to set API URL:", previous, error);
             this.url = previous;
         }
-    } 
+    }
 
     setIndexes = (indexes) => {
-        if(!indexes){
-            this.indexes = INDEXES.map(idx=>idx);
+        if (!indexes) {
+            this.indexes = INDEXES.map(idx => idx);
         }
         indexes.forEach(idx => {
             idx = idx.toLowerCase();
-            if(INDEXES.indexOf(idx) === -1){
+            if (INDEXES.indexOf(idx) === -1) {
                 throw new Error(`Unsupported index: ${idx}. Should be one of: ${INDEXES.join(", ")}.`);
             }
         })
-        this.indexes = indexes.map(idx=>idx.toLowerCase());
+        this.indexes = indexes.map(idx => idx.toLowerCase());
     }
 
-    _api = async(endpoint, params=null)=>{
+    _api = async (endpoint, params = null) => {
         let url = this.url + endpoint;
         if (params) url += "?" + new URLSearchParams(params);
-        try{
+        try {
             const response = await fetch(url);
             //console.log("API Response", response);
             //console.log("response", response.ok);
             if (!response.ok) throw new Error('API Error');
             return response.json();
         }
-        catch(error){
+        catch (error) {
             console.warn("Localisation API Error:", endpoint, params);
             throw error;
         }
     }
 
-    load = async (force=false) => {
-        if(!this._promise_capabilities || force){
+    load = async (force = false) => {
+        if (!this._promise_capabilities || force) {
             this._promise_capabilities = this._api("getCapabilities");
-            this._promise_capabilities = this._promise_capabilities.then(c=>{
+            this._promise_capabilities = this._promise_capabilities.then(c => {
                 this.capabilities = c;
             });
         }
         return this._promise_capabilities;
     }
 
-    search = async(text, location) => {
+    search = async (text, location) => {
         let params = {
             q: text,
             index: this.indexes.join(","),
             limit: this.options.limit
         }
-        if(location){
+        if (location) {
             params.lon = location.longitude || location.lon || location.lng;
             params.lat = location.lattitude || location.lat;
         }
         return this._api("search", params);
     }
 
-    complete = async (text, location)=>{
-        if(!text) return [];
-        if(!text.trim() || text.length < 3) return [];
+    complete = async (text, location) => {
+        if (!text) return [];
+        if (!text.trim() || text.length < 3) return [];
         let params = {
             text,
             type: getAutocompleteTypes(this.indexes).join(","),
             limit: this.options.limit
         }
-        if(location){
+        if (location) {
             params.lon = location.longitude || location.lon || location.lng;
             params.lat = location.lattitude || location.lat;
         }
-       const data = await this._api("completion", params);
-       return data.results
+        const data = await this._api("completion", params);
+        return data.results
     }
 
-    reverse = async(location) => {
+    reverse = async (location) => {
         let params = {
             lon: location.longitude || location.lon || location.lng,
             lat: location.lattitude || location.lat,
